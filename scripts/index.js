f763d08ce01bb65f2c59beb0c5a8d4bb1a2b833c
Aleksandr Smetanin <smetanin-sanya2015@yandex.ru>
2024-04-08 21:30:05 +0300

    review 1

--- scripts/index.js
+++ scripts/index.js
@@ -2,31 +2,32 @@ const cardTemplate = document.querySelector("#card-template").content;
 
 function addCard(info, delCallBack) {
   const cardElement = cardTemplate
-    .querySelector(".places__item ")
+    .querySelector(".places__item")
     .cloneNode(true);
 
-  cardElement.querySelector(".card__image").src = info.link;
-  cardElement.querySelector(".card__image").alt = info.name;
-  cardElement.querySelector(".card__title").textContent = info.name;
+  cardElement.querySelector(".card__image").src = info.link; // добавляем картинку в проект
+  cardElement.querySelector(".card__image").alt = info.name; // добавляем текст для скринридера
+  cardElement.querySelector(".card__title").textContent = info.name; // добавляем title под картинкой
 
   const resetButton = cardElement.querySelector(".card__delete-button");
   resetButton.addEventListener("click", () => {
-    delCallBack(cardElement);
+    const listItem = resetButton.closest(".card"); // создаем переменную которая будет принимать callback на удаление карточки
+    delCallBack(listItem);
   });
-
   return cardElement;
 }
 
-function cleanerCard(card) {
-  card.remove();
+// фун=ция для удаления карточки
+function cleanerCard(listItem) {
+  listItem.remove();
 }
 
-function renderCards() {
-  const placesList = document.querySelector(".places__list");
-  initialCards.forEach(function (element) {
-    cardElement = addCard(element, cleanerCard);
-    placesList.append(cardElement);
+// функция для формирования новых карточек.
+function renderLiCards() {
+  const placeList = document.querySelector(".places__list");
+  initialCards.forEach(function (item) {
+    cardElement = addCard(item, cleanerCard);
+    placeList.append(cardElement);
   });
 }
-
-renderCards();
+renderLiCards();
